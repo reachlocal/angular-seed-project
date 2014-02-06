ae7ed826d581eb28f94d0d4f0e699a3a23bf1dd7
Mauricio Sanches <msanches@thoughtworks.com>
2014-02-06 15:34:59 -0800

    [CPI-20] adding angular ui bootstrap. using collapsible elements for ad groups on global nav.

--- app/modules/main/directives/rlAdGroupFilterNav.js
+++ app/modules/main/directives/rlAdGroupFilterNav.js
@@ -1,12 +1,37 @@
 angular
-    .rlmodule('rl.cpi.main.directives.rlAdGroupFilterNav', ['rl.cpi.main.services.adGroups'])
+    .rlmodule('rl.cpi.main.directives.rlAdGroupFilterNav', ['ui.bootstrap', 'rl.cpi.main.services.adGroups'])
     .directive('rlAdGroupFilterNav', function () {
         return {
             templateUrl: "modules/main/directives/rlAdGroupFilterNav.html",
             scope: {},
             restrict: 'E',
             controller: function ($scope, AdGroups) {
-                $scope.publishers = AdGroups.get();
+//                $scope.publishers = AdGroups.get();
+                $scope.publishers = [{
+                    name: "Google",
+                    adGroups: [{
+                        name: "Local Roto-rooting",
+                        id:  123
+                    }, {
+                        name: "DMA Roto-rooting",
+                        id: 456
+                    }]
+                }, {
+                    name: "Bing",
+                    adGroups: [{
+                        name: "Local Roto-rooting",
+                        id:  123
+                    }]
+                }, {
+                    name: "ALL",
+                    adGroups: [{
+                        name: "Local Roto-rooting",
+                        id:  123
+                    }, {
+                        name: "DMA Roto-rooting",
+                        id: 456
+                    }]
+                }]
             }
         };
 
