5c455e6d40758d5f79d0d4953dda17d11d8c000d
Tom Hood <tom.hood@reachlocal.com>
2014-02-14 11:00:30 -0800

    chore: remove sample files left over from inception
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   Gulpfile.js
    #	modified:   app/modules/main/config.js
    #	modified:   ci/gulpConfig.js
    #	modified:   test/features/steps/global_navigation.js
    #

--- app/modules/main/controllers/sample.js
+++
@@ -1,8 +0,0 @@
-/*jslint browser: true*/
-/*global angular*/
-angular.rlmodule('rl.cpi.main.controllers.sample', []).
-    controller('Sample', function($scope) {
-        $scope.sample = 'Your app is working.';
-        console.log($scope.sample);
-        $scope.sample2 = 'This is a test variable for the spec file.';
-    });
