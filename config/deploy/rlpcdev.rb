5cbf3382a51e59503b2c065c03e4eb61ab96ebd1
Carlos Villela <cvillela@thoughtworks.com>
2014-02-25 18:03:51 -0300

    refactor(CPI-64): slightly less hacky, but still need to fix the config

--- config/deploy/rlpcdev.rb
+++ config/deploy/rlpcdev.rb
@@ -17,14 +17,25 @@ server "www.#{ENV['OS_USERNAME']}.cpi.dev.wh.reachlocal.com", roles: [:web]
 namespace :deploy do
   after :updated, :dist do
     on roles :web do
-      within release_path do
-        # Temporary hack to change the config; this will be removed when config
-        # is revisited in cpi-client
-        execute %[sed -i 's/gatewayBaseUrl: "http:\\/\\/localhost:8001"/gatewayBaseUrl: "http:\\/\\/gateway.#{ENV['OS_USERNAME']}.cpi.dev.wh.reachlocal.com"/' #{release_path}/app/modules/main/config.js]
+      # Temporary hack to change the config; this will be removed when config
+      # is revisited in cpi-client
+      execute %[sed -i 's/gatewayBaseUrl: "http:\\/\\/localhost:8001"/gatewayBaseUrl: "http:\\/\\/gateway.#{ENV['OS_USERNAME']}.cpi.dev.wh.reachlocal.com"/' #{release_path}/app/modules/main/config.js]
 
-        # Generate the 'dist' directory
-        execute './ci/default.sh'
-      end
+      # Installs NVM and uses Node 0.10
+      execute <<-EOC
+        cd #{release_path}
+        wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
+      EOC
+
+      execute <<-EOC
+        cd #{release_path}
+        source $HOME/.nvm/nvm.sh
+        nvm install v0.10
+        nvm alias default 0.10
+        nvm use 0.10
+        npm install --quiet
+        ./node_modules/.bin/gulp dist
+      EOC
     end
   end
 end
