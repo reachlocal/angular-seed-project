dc894533ea2d3ae572066e66f41e03e072de0c43
Mauricio Sanches <msanches@thoughtworks.com>
2014-02-24 16:33:52 -0800

    [CPI-22] adding surround with parenthesis filter to deal with geo type text at left nav bar.

---
+++ app/modules/main/filters/surroundWithParenthesis.js
@@ -0,0 +1,8 @@
+angular
+  .rlmodule('rl.cpi.main.filters.SurroundWithParenthesis', [])
+  .filter('surroundWithParenthesis', function() {
+    return function(input) {
+      if (!input) { return input; }
+      return "(" + input + ")";
+    };
+  });
