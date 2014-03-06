f49304a087caf995375b3833bce1b936c8eeb1c5
Felipe Mobus <fmobus@thoughtworks.com>
2014-03-06 18:59:34 -0300

    [CPI-26] refactoring(rlRecommendation): rl-recommendation directive becomes rl-recommendations
    and also handles the behaviour of the well done message banner
    
    as well as the counter of active recommendations

---
+++ app/modules/main/directives/rlRecommendations/rlRecommendations.js
@@ -0,0 +1,12 @@
+angular
+    .rlmodule('rl.cpi.main.directives.rlRecommendations', ['ui.bootstrap'])
+    .directive('rlRecommendations', function () {
+        return {
+            templateUrl: "modules/main/directives/rlRecommendations/rlRecommendations.html",
+            scope: {
+                expanded: '=?',
+                recommendations: '='
+            },
+            restrict: 'E'
+        };
+    })
