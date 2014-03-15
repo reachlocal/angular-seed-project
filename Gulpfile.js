c0fe1e0f19ca82b267e804edae822ca8944b7e29
Tom Hood <tom.hood@reachlocal.com>
2014-03-14 18:42:21 -0700

    feat(git): add gulp task to install pre-commit hook
    Add task "githook" to install a pre-commit hook
    The pre-commit hook will fail the commit if the build is broken
    Per Shrikar's request, this hook is installed rather aggressively (on build)

--- Gulpfile.js
+++ Gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('default', ['build']);
 /**
  * Build task:  Run tests and compile assets into 'dist/' directory
  */
-gulp.task('build', ['dist', 'test', 'lint:failhard']);
+gulp.task('build', ['dist', 'test', 'lint:failhard', 'githook']);
 
 /**
  * Magic loader that pulls in all tasks from the gulp_tasks directory
