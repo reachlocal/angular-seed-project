d8f99ec2d8dbc1cf0470a46f87fec0bb06d59b46
Tom Hood <tom.hood@reachlocal.com>
2014-03-24 12:46:32 -0700

    refactor(rlPublisherFilterNav): conform to new dir convention
    Move rlPublisherFilterNav and rlPublisherMenuItem directives to
    conform to new naming convention and tidy up the directives dir

--- app/modules/main/directives/rlPublisherFilterNav/rlPublisherMenuItem/rlPublisherMenuItem.js
+++ app/modules/main/directives/rlPublisherFilterNav/rlPublisherMenuItem/rlPublisherMenuItem.js
@@ -2,7 +2,7 @@ angular
     .rlmodule('rl.cpi.main.directives.rlPublisherMenuItem', ['ui.bootstrap'])
     .directive('rlPublisherMenuItem', function () {
         return {
-            templateUrl: "modules/main/directives/rlPublisherMenuItem.html",
+            templateUrl: "modules/main/directives/rlPublisherFilterNav/rlPublisherMenuItem/rlPublisherMenuItem.html",
             restrict: 'E',
             scope: { publisher: "=item" }
         };
