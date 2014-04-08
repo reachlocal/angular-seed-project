ac14ba7b928325f95e08f55b90e3e5835a16bab5
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2014-04-08 14:18:40 -0300

    Fixes identation.

--- gulp_tasks/rl_seeds.js
+++ gulp_tasks/rl_seeds.js
@@ -2,22 +2,22 @@ var gulp = require('gulp');
 var config = require('./config/config');
 
 var paths = {
-  source      : config.APPLICATION_ROOT + '/modules/rl_seeds',
-  destination : config.APPLICATION_ROOT + '/styleguide',
-  dist        : config.MINIFY_DESTINATION + '/styleguide'
+    source      : config.APPLICATION_ROOT + '/modules/rl_seeds',
+    destination : config.APPLICATION_ROOT + '/styleguide',
+    dist        : config.MINIFY_DESTINATION + '/styleguide'
 };
 
 gulp.task('styleguide:index', function () {
-  gulp.src(paths.source + '/index.html').pipe(gulp.dest(paths.destination));
+    gulp.src(paths.source + '/index.html').pipe(gulp.dest(paths.destination));
 });
 
 gulp.task('styleguide', ['styleguide:index'], function () {
-  var sass = require('gulp-sass');
-  gulp.src(paths.source + '/seeds.scss')
-    .pipe(sass({ sourceComments: 'map' }))
-    .pipe(gulp.dest(paths.destination));
+    var sass = require('gulp-sass');
+    gulp.src(paths.source + '/seeds.scss')
+        .pipe(sass({ sourceComments: 'map' }))
+        .pipe(gulp.dest(paths.destination));
 });
 
 gulp.task('dist:styleguide', ['styleguide'], function () {
-  gulp.src(paths.destination + '/**/*').pipe(gulp.dest(paths.dist));
+    gulp.src(paths.destination + '/**/*').pipe(gulp.dest(paths.dist));
 });
