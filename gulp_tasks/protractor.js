4539e7d57e0e4f8ff513e17b9e1e9957d3cad0f4
Nicholas Pufal <npufal@thoughtworks.com>
2014-04-24 15:28:33 -0300

    fix(protractor_debug) the old task was hanging on the webdriver step so now its launching webdriver directly

--- gulp_tasks/protractor.js
+++ gulp_tasks/protractor.js
@@ -1,21 +1,18 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
-/**
- * Static web server AND mock rest web server
- **/
 gulp.task('protractor:debug', ['serve'], function() {
-    var webDriver = require('./lib/webDriver');
-    var runDebugger = function() {
-        var child_process = require('child_process');
-        var options = ['node_modules/protractor/bin/elementexplorer.js', 'http://localhost:4000/#/campaign/1422307'];
-//        var cProcess = child_process.spawn('node', options);
-//
-//        cProcess.stdout.pipe(process.stdout);
-//        cProcess.stderr.pipe(process.stderr);
-//        process.stdin.pipe(cProcess.stdin);
-        var command = ['node'].concat(options).join(' ');
-        gutil.log(gutil.colors.red("Please run this command in another terminal: "), command);
-    };
-    webDriver.startWebDriver(runDebugger);
+  runSelenium(runElementExplorer);
 });
+
+var runSelenium = function (callback) {
+  require('child_process').exec('node_modules/.bin/webdriver-manager start --chrome').stdout.on('data', function (data) {
+    if (data.match(/Started SocketListener/))
+      callback();
+  });
+};
+
+var runElementExplorer = function () {
+  commands = ['node_modules/protractor/bin/elementexplorer.js', 'http://localhost:4000/#/campaign/713896'];
+  gutil.log(gutil.colors.red("Please run this command in another terminal: "), commands.join(' '));
+};
