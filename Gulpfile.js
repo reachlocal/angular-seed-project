0f804c7def2c499b67d10c16ef6d111cd76e84fc
Srikar Ayilavarapu <asrikar@gmail.com>
2014-04-25 10:15:34 -0700

    chore(gulp): throw better message when npm module missing

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,13 @@
-var gulp  = require('gulp');
-require('require-dir')('./gulp_tasks');
-gulp.task('default', ['build', 'test', 'lint:failhard', 'githook']);
+try {
+    var gulp = require('gulp');
+    require('require-dir')('./gulp_tasks');
+    gulp.task('default', ['build', 'test', 'lint:failhard', 'githook']);
+} catch (err) {
+    if (err.code === 'MODULE_NOT_FOUND') {
+        console.log("#########################################");
+        console.log("You are missing some node modules...");
+        console.log("Please run 'npm install', then try again.");
+        console.log("#########################################");
+        throw err;
+    }
+}
