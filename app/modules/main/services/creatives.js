459a7bb1371ebe4bf206e5e4b8e341acd47adedc
Srikar Ayilavarapu <asrikar@gmail.com>
2014-04-28 18:58:51 -0700

    CPI-161 Showing 'Cost Per Lead' and 'Total Cost' in the data table

--- app/modules/main/services/creatives.js
+++ app/modules/main/services/creatives.js
@@ -9,16 +9,26 @@ angular
     })
     .factory('CreativeHeaders', function() {
         return {
-            impressionsShare: {
-                name: "Impression Share",
-                shortName: "iShare",
-                format: "percent"
-            },
             impressions: {
                 name: "Impressions",
                 shortName: "Imp",
                 description: "An impression is counted every time the creative is displayed",
                 format: "integer"
+            },
+            totalCost: {
+                name: "Total Cost",
+                shortName: "Cost",
+                format: "decimal"
+            },
+            leads: {
+                name: "Lead Count",
+                shortName: "LEADS",
+                format: "integer"
+            },
+            costPerLead: {
+                name: "Cost Per Lead",
+                shortName: "CPL",
+                format: "decimal"
             },
             clicks: {
                 name: "Clicks",
@@ -30,11 +40,6 @@ angular
                 shortName: "CTR",
                 description: "Click through rate",
                 format: "percent"
-            },
-            leadCount: {
-                name: "Lead Count",
-                shortName: "LEADS",
-                format: "integer"
             },
             conversions: {
                 name: "Conversions",
