a1d8a3b9d86e649659eeade80e6900814c9447ba
Tom Hood <tom.hood@reachlocal.com>
2014-05-01 12:16:50 -0700

    feat(calendar): add 'Current mont' selection
    -m  Closes: CPI-143

--- app/modules/main/directives/rlPublisherFilterNav/rlPublisherFilterNav.js
+++ app/modules/main/directives/rlPublisherFilterNav/rlPublisherFilterNav.js
@@ -8,6 +8,15 @@ angular
         function last(amount, granularity) {
             return Moment.build().subtract(granularity, amount).format(dateFormat);
         }
+        // Return { from: [first day of current month], to: [last day of current month] }
+        function currentMonth() {
+            var thisMonth = Moment.build();
+            var monthNumber = thisMonth.month();
+            var daysInMonth = thisMonth.daysInMonth();
+            var firstDay = Moment.build().month(monthNumber).date(1);
+            var lastDay = Moment.build().month(monthNumber).date(daysInMonth);
+            return { from: firstDay.format(dateFormat), to: lastDay.format(dateFormat) };
+        }
         var last30Days = { from: last(30, 'days'), to: today };
         var initialRange = last30Days;
         if (!!QueryParams.to) {
@@ -19,6 +28,8 @@ angular
         $scope.dateRangeOptions = {
             customLabel: 'rlPublisherFilterNav.dateRange.Custom',
             ranges: [
+                { label: 'rlPublisherFilterNav.dateRange.Current month',
+                    value: currentMonth() },
                 { label: 'rlPublisherFilterNav.dateRange.Last 30 days',
                     value: last30Days },
                 { label: 'rlPublisherFilterNav.dateRange.Last 60 days',
