853c60cff8e4b308acad3bd9b0f9840800eda29a
Tom Hood <tom.hood@reachlocal.com>
2014-05-12 10:56:00 -0700

    refactor(airbrake): clean up airbrake shim

--- app/airbrake-shim.js
+++ app/airbrake-shim.js
@@ -1,58 +1,47 @@
-(function(window) {
+(function bootStrapAirbrake(window) {
 
-// Airbrake shim that stores exceptions until Airbrake notifier is loaded.
-window.Airbrake = [];
+  // This will show up in our error reports
+  var environment = location.hostname;
 
-// Wraps passed function and returns new function that catches and
-// reports unhandled exceptions.
-Airbrake.wrap = function(fn) {
-  var airbrakeWrapper = function() {
-    try {
-      return fn.apply(this, arguments);
-    } catch (exc) {
-      args = Array.prototype.slice.call(arguments);
-      Airbrake.push({error: exc, params: {arguments: args}});
+  // Airbrake shim that stores exceptions until Airbrake notifier is loaded.
+  var Airbrake = [];
+  window.Airbrake = Airbrake;
+
+  // Registers the environment name
+  Airbrake.onload = function (client) {
+    client.setEnvironmentName(environment);
+  };
+
+  // Reports unhandled exceptions (everything else is wired up with angular's
+  // Exception handler - see airbrake.js in the config dir
+  window.onerror = function airbrakeGlobalExceptionHandler(message, file, line, column, error) {
+    if (error) {
+      Airbrake.push({error: error});
+    } else {
+      Airbrake.push({error: {
+        message: message,
+        fileName: file,
+        lineNumber: line,
+        columnNumber: column || 0
+      }});
     }
   };
-  if (fn.guid) {
-    airbrakeWrapper.guid = fn.guid;
-  }
-  return airbrakeWrapper;
-};
 
-// Registers console reporter when notifier is ready.
-Airbrake.onload = function(client) {
-  //Airbrake.addReporter(Airbrake.consoleReporter);
-  client.setEnvironmentName(location.hostname);
-};
-
-// Reports unhandled exceptions.
-window.onerror = function(message, file, line, column, error) {
-  if (error) {
-    Airbrake.push({error: error});
-  } else {
-    Airbrake.push({error: {
-      message: message,
-      fileName: file,
-      lineNumber: line,
-      columnNumber: column || 0
-    }});
-  }
-};
-
-var loadAirbrakeNotifier = function() {
-  var script = document.createElement('script'),
+  function loadAirbrakeNotifier() {
+    var script = document.createElement('script'),
       sibling = document.getElementsByTagName('script')[0];
-  script.src = 'https://ssljscdn.airbrake.io/0.3/airbrake.min.js';
-  script.async = true;
-  sibling.parentNode.insertBefore(script, sibling);
-};
+    script.src = 'https://ssljscdn.airbrake.io/0.3/airbrake.min.js';
+    script.async = true;
+    sibling.parentNode.insertBefore(script, sibling);
+  }
 
-// Asynchronously loads Airbrake notifier.
-if (window.addEventListener) {
-  window.addEventListener('load', loadAirbrakeNotifier, false);
-} else {
-  window.attachEvent('onload', loadAirbrakeNotifier);
-}
+  if (location.hostname !== 'localhost') {
+    // Asynchronously loads Airbrake notifier.
+    if (window.addEventListener) {
+      window.addEventListener('load', loadAirbrakeNotifier, false);
+    } else {
+      window.attachEvent('onload', loadAirbrakeNotifier);
+    }
+  }
 
-})(window);
+}(window));
