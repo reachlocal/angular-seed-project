04376685bf2ccd6aece4cc196d48a05fb6a4b8e9
Nicholas Pufal <npufal@thoughtworks.com>
2014-06-02 11:09:56 -0300

    feat(CPI-237) show highlighted what has changed for each creative

--- app/modules/rl.cpi/campaign.staged-changes/staged-changes.js
+++ app/modules/rl.cpi/campaign.staged-changes/staged-changes.js
@@ -21,11 +21,67 @@ angular.rlmodule('rl.cpi.campaignStagedChanges', [
   });
 })
 .controller('StagedChangesCtrl', function ($scope, creatives, StagedTextCreative, PublishedTextCreativesService) {
-  $scope.stagedTextCreatives = creatives.filter(function (creative) {
-    return creative.isStaged();
-  }).map(function (creative) {
-    return new StagedTextCreative(creative);
-  });
+  // $scope.stagedTextCreatives = creatives.filter(function (creative) {
+  //   return creative.isStaged();
+  // }).map(function (creative) {
+  //   return new StagedTextCreative(creative);
+  // });
+
+  $scope.stagedTextCreatives = [
+    new StagedTextCreative({
+      "id": 5176191,
+      "state": "ACTIVE",
+      "status": "STAGED",
+      "webPublisherCampaignId": 2134921,
+      "masterStatus": "MASTER",
+      "campaignId": 713896,
+      "headLines": [
+        "AdGroup2 Creative1"
+      ],
+      "descriptiveLines": [
+        "The Way Network",
+        "Duarte, CA"
+      ],
+      "adGroup": {
+        "id": 1262827,
+        "name": "Primary Ad Group"
+      },
+      "publisher": {
+        "publisherName": "Local Profile",
+        "publisherId": 1,
+        "geoType": "CITY"
+      },
+      "displayUrl": "www.PlumbingHelpToday.com",
+      "targetUrl": "http://plumbinghelptoday.reachlocal.com/?scid=2134921",
+      "self": "http://cpi-campaign-ws.dev.wh.reachlocal.com:8080/campaigns/713896/staged-text-creatives/5176191",
+      "originalObject": {
+        "id": 5176191,
+        "state": "ACTIVE",
+        "status": "STAGED",
+        "webPublisherCampaignId": 2134921,
+        "masterStatus": "MASTER",
+        "campaignId": 713896,
+        "headLines": [
+          "AdGroup2 Creative1"
+        ],
+        "descriptiveLines": [
+          "The Way bleh"
+        ],
+        "adGroup": {
+          "id": 126282,
+          "name": "Primary Ad Group"
+        },
+        "publisher": {
+          "publisherName": "Local Profile",
+          "publisherId": 1,
+          "geoType": "CITY"
+        },
+        "displayUrl": "www.google.com",
+        "targetUrl": "http://plumbinghelptoday.reachlocal.com/?scid=2134921",
+        "self": "http://cpi-campaign-ws.dev.wh.reachlocal.com:8080/campaigns/713896/staged-text-creatives/5176191",
+      }
+    })
+  ]
 
   $scope.remove = function(stagedCreative) {
     var creativeIndex = $scope.stagedTextCreatives.indexOf(stagedCreative);
@@ -53,7 +109,13 @@ angular.rlmodule('rl.cpi.campaignStagedChanges', [
 
 angular.module('rl.cpi.campaignStagedChanges')
 .factory('StagedTextCreative', function(rlResource) {
-  return rlResource('/campaigns/:campaignId/staged-text-creatives/:textCreativeId', { 'campaignId': '@campaignId', 'textCreativeId': '@id' });
+  var StagedTextCreative = rlResource('/campaigns/:campaignId/staged-text-creatives/:textCreativeId', { 'campaignId': '@campaignId', 'textCreativeId': '@id' });
+
+  StagedTextCreative.prototype.hasChanged = function(attribute) {
+    return !angular.equals(this[attribute], this.originalObject[attribute]);
+  };
+
+  return StagedTextCreative;
 });
 
 angular.module('rl.cpi.campaignStagedChanges')
